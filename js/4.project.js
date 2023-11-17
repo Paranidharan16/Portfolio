d7b610f82ee2416d0ff6740f5627438c766e132c
Paranidharan P <147366655+Paranidharan16@users.noreply.github.com>
2023-11-17 10:06:41 +0530

    Update 4.project.js

--- js/4.project.js
+++ js/4.project.js
@@ -15,100 +15,20 @@ export function project() {
   });
 
   projectObserv.observe(project);
-
-  const ytc = document.querySelector(".ytc");
-  const td = document.querySelector(".td");
-  const fcd = document.querySelector(".fcd");
-  const rps = document.querySelector(".rps");
-  const fh = document.querySelector(".fh");
-  const cal = document.querySelector(".cal");
-  // YouTube Observation
-
-  const youTube = new IntersectionObserver(youTubeFunction, {
-    root: null,
-    threshold: 0.7,
-  });
-  function youTubeFunction(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      ytc.classList.remove("blur");
-      youTube.unobserve(ytc);
-    }
-  }
-  youTube.observe(ytc);
-
-  // Todo-List Observaton
-
-  const todoList = new IntersectionObserver(todoFunction, {
-    root: null,
-    threshold: 0.7,
-  });
-  function todoFunction(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      td.classList.remove("blur");
-      todoList.unobserve(td);
-    }
-  }
-  todoList.observe(td);
-
-  // Fetch-Country-Data Observation
-
-  const fetchCountryData = new IntersectionObserver(fetchCountryDataFunction, {
-    root: null,
-    threshold: 0.7,
-  });
-  function fetchCountryDataFunction(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      fcd.classList.remove("blur");
-      fetchCountryData.unobserve(fcd);
-    }
-  }
-  fetchCountryData.observe(fcd);
-
-  // Rock-Paper-Sisor Observation
-
-  const rock = new IntersectionObserver(rockFunction, {
-    root: null,
-    threshold: 0.7,
-  });
-  function rockFunction(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      rps.classList.remove("blur");
-      rock.unobserve(rps);
-    }
-  }
-  rock.observe(rps);
-
-  // Fresh-Happiness observation
-
-  const fresh = new IntersectionObserver(freshFunction, {
-    root: null,
-    threshold: 0.7,
-  });
-  function freshFunction(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      fh.classList.remove("blur");
-      fresh.unobserve(fh);
-    }
-  }
-  fresh.observe(fh);
-
-  // Calculator observation
-
-  const calculator = new IntersectionObserver(calFunction, {
-    root: null,
-    threshold: 0.7,
-  });
-  function calFunction(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      cal.classList.remove("blur");
-      calculator.unobserve(cal);
-    }
-  }
-  calculator.observe(cal);
 }
+
+const allProject = document.querySelectorAll(".pro");
+allProject.forEach((e) => {
+  function all(entry) {
+    const [data] = entry;
+    if (data.isIntersecting) {
+      data.target.classList.remove("blur");
+      obs.unobserve(e);
+    }
+  }
+  const obs = new IntersectionObserver(all, {
+    root: null,
+    threshold: 0.7,
+  });
+  obs.observe(e);
+});
