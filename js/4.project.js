ca041923fdb22bea59c763feca115aa1da431c1c
Paranidharan P <147366655+Paranidharan16@users.noreply.github.com>
2023-11-17 10:11:15 +0530

    Update 4.project.js

--- js/4.project.js
+++ js/4.project.js
@@ -2,6 +2,7 @@ export function project() {
   const project = document.querySelector(".project-title");
 
   function projectObservFunction(entry) {
+    console.log(entry);
     const [intersect] = entry;
     if (intersect.isIntersecting) {
       project.classList.remove("visibility");
@@ -13,22 +14,21 @@ export function project() {
     root: null,
     threshold: 0.6,
   });
-
   projectObserv.observe(project);
-}
 
-const allProject = document.querySelectorAll(".pro");
-allProject.forEach((e) => {
-  function all(entry) {
-    const [data] = entry;
-    if (data.isIntersecting) {
-      data.target.classList.remove("blur");
-      obs.unobserve(e);
+  const allProject = document.querySelectorAll(".pro");
+  allProject.forEach((e) => {
+    function all(entry) {
+      const [data] = entry;
+      if (data.isIntersecting) {
+        data.target.classList.remove("blur");
+        obs.unobserve(e);
+      }
     }
-  }
-  const obs = new IntersectionObserver(all, {
-    root: null,
-    threshold: 0.7,
+    const obs = new IntersectionObserver(all, {
+      root: null,
+      threshold: 0.7,
+    });
+    obs.observe(e);
   });
-  obs.observe(e);
-});
+}
